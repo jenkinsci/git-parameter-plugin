5ce25f11509f8c3990bbc56330212451793e6af1
Gabor Liptak <gliptak@gmail.com>
2013-01-29 09:43:04 -0500

    Remove unused import

--- src/main/java/net/uaznia/lukanus/hudson/plugins/gitparameter/GitParameterValue.java
+++ src/main/java/net/uaznia/lukanus/hudson/plugins/gitparameter/GitParameterValue.java
@@ -1,7 +1,5 @@
 package net.uaznia.lukanus.hudson.plugins.gitparameter;
 
-import hudson.EnvVars;
-import hudson.model.AbstractBuild;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import hudson.model.StringParameterValue;
