59e6273537f2c3f8061517a18f9460186efb6d18
Damien Duportal <damien.duportal@gmail.com>
2025-03-04 12:27:47 +0100

    Revert https://github.com/jenkinsci/git-parameter-plugin/pull/145/commits/b89550f51a7e996779192e126a3d487f4e0a4bcb for https://github.com/jenkins-infra/helpdesk/issues/4568.
    This change allows the Jenkins Infra team to retry Windows builds and test the encoding settings.
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- src/test/java/net/uaznia/lukanus/hudson/plugins/gitparameter/GitParameterDefinitionTest.java
+++ src/test/java/net/uaznia/lukanus/hudson/plugins/gitparameter/GitParameterDefinitionTest.java
@@ -398,13 +398,8 @@ public class GitParameterDefinitionTest {
         assertNotNull(build);
         ItemsErrorModel items = def.getDescriptor().doFillValueItems(project, def.getName());
         assertTrue(isListBoxItem(items, "00a8385cba1e4e32cf823775e2b3dbe5eb27931d"));
-        if (!Functions.isWindows() || System.getenv("CI") == null) {
-            // Windows agents on AWS ci.jenkins.io changed character set configuration compared to Azure
-            // Only test when not on Windows and not on a CI configuration
-            // TODO: Remove conditional when AWS ci.jenkins.io agent character set config is updated
-            assertTrue(isListBoxItemName(
-                    items, "00a8385c 2011-10-30 17:11 Łukasz Miłkowski <lukanus@uaznia.net> initial readme"));
-        }
+        assertTrue(isListBoxItemName(
+                items, "00a8385c 2011-10-30 17:11 Łukasz Miłkowski <lukanus@uaznia.net> initial readme"));
     }
 
     @Test
